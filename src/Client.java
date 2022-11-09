92d64241958b8be949e2149bacc901720e49f282
Nithin-anim <nithin@animaker.com>
2022-11-09 10:24:52 -0500

    Implement authentication

--- src/Client.java
+++ src/Client.java
@@ -4,27 +4,70 @@ import java.util.Scanner;
 
 public class Client {
     private static final int PORT = 5000;
+    private static Socket socket = null;
     private static DataOutputStream outputStream = null;
     private static Scanner scanner = new Scanner(System.in);
 
     public static void main(String[] args) {
         System.out.println("---FTP Client---");
         try {
-            Socket socket = new Socket("localhost", PORT);
-            System.out.println("Connected to server on port 5000");
-            outputStream = new DataOutputStream(socket.getOutputStream());
-            System.out.println("Enter path of file to upload:");
-            String path = scanner.nextLine();
-            System.out.println("Enter output file name:(output.png)");
-            String outputFile = scanner.nextLine();
-            sendFile(path, outputFile);
-
-            outputStream.close();
-            socket.close();
+            System.out.println("Enter 1 to signin and 2 to sign up");
+            String option = scanner.nextLine();
+            if (option.equals("1")) {
+                signIn();
+            }
+            if (option.equals("2")) {
+                signUp();
+            }
 
         } catch (Exception e) {
             System.out.println("Error" + e.toString());
         }
+    }
+
+    private static void signIn() {
+        try {
+            System.out.println("Enter username:");
+            String username = scanner.nextLine();
+            System.out.println("Enter password:");
+            String password = scanner.nextLine();
+
+            socket = new Socket("localhost", PORT);
+            System.out.println("Connected to server on port " + PORT);
+            outputStream = new DataOutputStream(socket.getOutputStream());
+            outputStream.writeUTF("1:" + username + ":" + password);
+            proceedWithFileTransfer();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    private static void signUp() {
+        try {
+            System.out.println("Enter username:");
+            String username = scanner.nextLine();
+            System.out.println("Enter password:");
+            String password = scanner.nextLine();
+
+            socket = new Socket("localhost", PORT);
+            System.out.println("Connected to server on port " + PORT);
+            outputStream = new DataOutputStream(socket.getOutputStream());
+            outputStream.writeUTF("2:" + username + ":" + password);
+            proceedWithFileTransfer();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    private static void proceedWithFileTransfer() throws Exception {
+        System.out.println("Enter path of file to upload:");
+        String path = scanner.nextLine();
+        System.out.println("Enter output file name:(output.png)");
+        String outputFile = scanner.nextLine();
+        sendFile(path, outputFile);
+
+        outputStream.close();
+        socket.close();
     }
 
     private static void sendFile(String path, String outputFile) {
@@ -38,7 +81,7 @@ public class Client {
             while ((bytes = fileInputStream.read(buffer)) != -1) {
                 outputStream.write(buffer, 0, bytes);
                 outputStream.flush();
-                System.out.println("File written to stream...");
+                System.out.println("File written to stream");
             }
             fileInputStream.close();
         } catch (Exception e) {
--- src/Server.java
+++ src/Server.java
@@ -1,29 +1,56 @@
 import java.io.*;
+import java.util.HashMap;
 import java.net.Socket;
 import java.net.ServerSocket;
 
 public class Server {
+    private static final int PORT = 5000;
     private static DataInputStream inputStream = null;
+    private static FTPAuthentication authenticator = null;
 
     public static void main(String[] args) {
         System.out.println("---FTP Server---");
         try {
-            ServerSocket serverSocket = new ServerSocket(5000);
+            ServerSocket serverSocket = new ServerSocket(PORT);
             System.out.println("Listening on port 5000");
             Socket clienSocket = serverSocket.accept();
             inputStream = new DataInputStream(clienSocket.getInputStream());
-
-            receiveFile("output.png");
+            boolean loggedIn = handleAuth();
+            if (loggedIn) {
+                receiveFile();
+            } else {
+                System.out.println("Error logging in user");
+            }
             inputStream.close();
             clienSocket.close();
             serverSocket.close();
 
         } catch (Exception e) {
-            System.out.println("Error" + e.toString());
+            e.printStackTrace();
         }
     }
 
-    private static void receiveFile(String path) {
+    private static boolean handleAuth() {
+        try {
+            String creds = inputStream.readUTF();
+            String[] credentials = creds.split(":");
+            if (credentials.length > 0) {
+                authenticator = new FTPAuthentication();
+                if (credentials[0].equals("1")) {
+                    return authenticator.signIn(credentials[1], credentials[2]);
+                } else {
+                    return authenticator.signUp(credentials[1], credentials[2]);
+                }
+            } else {
+                return false;
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+            return false;
+        }
+    }
+
+    private static void receiveFile() {
         try {
             long fileSize = inputStream.readLong();
             String outputFile = inputStream.readUTF();
@@ -35,12 +62,62 @@ public class Server {
                     && (bytes = inputStream.read(buffer, 0, (int) Math.min(buffer.length, fileSize))) != -1) {
                 fileOutputStream.write(buffer, 0, bytes);
                 fileSize -= bytes;
-                System.out.println("Writing file...");
+                System.out.println("Writing file");
             }
             System.out.println("File uploaded to server");
             fileOutputStream.close();
         } catch (Exception e) {
             System.out.println("Error in receiving file" + e.toString());
+        }
+    }
+}
+
+class FTPAuthentication {
+    private static FileWriter dbWriter = null;
+    private static FileReader dbReader = null;
+    private static HashMap<String, String> creds = null;
+
+    boolean signUp(String username, String password) {
+        try {
+            dbWriter = new FileWriter("creds.txt", true);
+            BufferedWriter writer = new BufferedWriter(dbWriter);
+            writer.write(username);
+            writer.write(":");
+            writer.write(password);
+            writer.newLine();
+            writer.close();
+            System.out.println("User has signed up and logged in");
+            return true;
+        } catch (Exception e) {
+            e.printStackTrace();
+            return false;
+        }
+    }
+
+    boolean signIn(String username, String password) {
+        fetchCredentials();
+        String fetchedPassword = creds.get(username);
+        if (fetchedPassword != null && fetchedPassword.equals(password)) {
+            System.out.println("User has been logged in");
+            return true;
+        } else {
+            System.out.println("Invaild credentials");
+            return false;
+        }
+    }
+
+    private void fetchCredentials() {
+        try {
+            dbReader = new FileReader("creds.txt");
+            BufferedReader reader = new BufferedReader(dbReader);
+            creds = new HashMap<String, String>();
+            String cred;
+            while ((cred = reader.readLine()) != null) {
+                String[] values = cred.split(":");
+                creds.put(values[0], values[1]);
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
         }
     }
 }
